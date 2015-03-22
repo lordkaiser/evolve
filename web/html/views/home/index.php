f173fd0d4e5ca421d9ffedcd6f821c9bd843e41b
lordkaiser <lordkaiser912@yahoo.com>
2015-03-23 00:37:56 +0800

    index upload

---
+++ web/html/views/home/index.php
@@ -0,0 +1,60 @@
+<div>
+	<div>
+		Sample Input
+		<span style="float: right;">
+			<select id="inputLang">
+				<?php foreach ($languages as $language) { ?>
+				<option value="<?php echo $language->L_ID;?>"><?php echo $language->name;?></option>
+				<?php } ?>
+			</select>
+		</span>
+	</div>
+	<div>
+		<textarea id="input" style="height: 200px; width: 100%;"></textarea>
+	</div>
+</div>
+
+
+<div>
+	<div>
+		Sample Output
+		<span style="float: right;">
+			<select id="outputLang">
+				<?php foreach ($languages as $language) { ?>
+				<option value="<?php echo $language->L_ID;?>"><?php echo $language->name;?></option>
+				<?php } ?>
+			</select>
+		</span>
+	</div>
+	<div>
+		<textarea id="output" style="height: 200px; width: 100%;"></textarea>
+	</div>
+</div>
+
+<div><button id="proceed">Decode</button></div>
+<div id="test"></div>
+<script type="text/javascript">
+	$('#proceed').click( function() {
+		$('#test').text('');
+		$.post('http://adapt.com/code/translateTest', {
+			lid: $('#inputLang').val(),
+			code: $('#input').val(),
+			target: $('#outputLang').val()
+		}, function(data) {	
+			$('#test').append(data);
+		});
+
+		$('#output').val('');
+		$.post('http://adapt.com/code/translate', {
+			lid: $('#inputLang').val(),
+			code: $('#input').val(),
+			target: $('#outputLang').val()
+		}, function(data) {
+			// console.log(data);
+			// data = data.replace("\\n","\n");
+			// console.log(data);
+			$('#output').val(data);
+			$('#output').val($('#output').val().replace(/\\n/g,"\n"));
+		});
+	});
+</script>
