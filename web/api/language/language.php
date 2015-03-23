cd1ac91c3c7a44cea1a5d346ccbaea555708cdb1
lordkaiser <lordkaiser912@yahoo.com>
2015-03-23 10:43:10 +0800

    lovey

--- web/api/language/language.php
+++ web/api/language/language.php
@@ -13,19 +13,7 @@ class language_API extends hapi {
 	}
 
 	public function constructDB() {
-		return $this->write("test:go", "-- phpMyAdmin SQL Dump
-			-- version 4.1.14
-			-- http://www.phpmyadmin.net
-			--
-			-- Host: 127.0.0.1
-			-- Generation Time: Mar 23, 2015 at 03:11 AM
-			-- Server version: 5.6.17
-			-- PHP Version: 5.5.12
-
-			SET SQL_MODE = \"NO_AUTO_VALUE_ON_ZERO\";
-			SET time_zone = \"+00:00\";
-
-
+		return $this->write("test:go", "
 			/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 			/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 			/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
