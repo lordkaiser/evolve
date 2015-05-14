bb864da890a51997770e5463c0486a11c3a7af41
lordkaiser <lordkaiser912@yahoo.com>
2015-05-14 10:14:10 +0800

    we can't kill real people

--- web/html/templates/wrapper.php
+++ web/html/templates/wrapper.php
@@ -42,13 +42,10 @@
 						</li>
 					</ul>
 				</div>
-				<form class="navbar-form navbar-right" action="http://www.manylists.com/mnl/search/" method="POST">
-				  <input type="text" class="form-control" id="searchbox" value="search listing..." name="searchText">
-				</form>
 			</div>
 		</nav>
 
-		<div class="container" style="background-color: rgb(250, 250, 250); min-height: 575px;">
+		<div class="container" style="background-color: rgb(250, 250, 250); min-height: 627px;">
 			<div style="clear: both; margin-top: 50px;">
 				<div id="content">
 					<?php echo $contents; ?>
@@ -56,39 +53,7 @@
 			</div>
 		</div>
 
-		<div style="min-height: 90px; width: 100%;">
-			<div style="background-color: #2f2f2f; min-height: 50px; width: 100%;">
-				<div class="container" style="padding-top: 15px;">
-					<div class="a-footer-block">
-						<span class="a-footer-title">
-							<a href="http://adapt.com/details/about-us" style="color: white;">
-								ABOUT ADAPT
-							</a>
-						</span>
-					</div>
-					<div class="a-footer-block">
-						<span class="a-footer-title">
-							<a href="http://adapt.com/details/terms" style="color: white;">
-								TERMS OF USE
-							</a>
-						</span>
-					</div>
-					<div class="a-footer-block">
-						<span class="a-footer-title">
-							<a href="http://adapt.com/details/contact-us" style="color: white;">
-								CONTACT US
-							</a>
-						</span>
-					</div>
-					<div class="a-footer-block" style="margin: 0px;">
-						<span class="a-footer-title">
-							<a href="http://adapt.com/details/privacy-policy" style="color: white;">
-								PRIVACY POLICY
-							</a>
-						</span>
-					</div>
-				</div>
-			</div>
+		<div style="min-height: 40px; width: 100%;">
 			<div style="background-color: #292929; min-height: 40px; width: 100%;">
 				<div class="container">
 					<div class="a-footer-description" style="padding: 10px 0;">Copyright © 2015 adapt.com All rights reserved.</div>
